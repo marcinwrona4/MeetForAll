153ded023276d31c25fda4443e770d9f00a2ca14
marcinwrona4 <44805682+marcinwrona4@users.noreply.github.com>
2019-03-14 19:27:55 +0100

    Create MeetforallApp.java

---
+++ src/main/java/meetforall/MeetforallApp.java
@@ -0,0 +1,13 @@
+package meetforall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+	@SpringBootApplication
+	public class MeetforallApp {
+
+	        public static void main(String[] args) {
+	            SpringApplication.run(MeetforallApp.class, args);
+	        }
+	 
+	}
